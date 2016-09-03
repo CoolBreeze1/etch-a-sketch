d0c6fdee662361157d8256091c13018fa9501310
reece baird <reeceybaird@hotmail.com>
2016-09-03 15:44:44 +0100

    before code clean up

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,24 +1,73 @@
 
 $(document).ready(function(){
 
-//create a table
-var $grid_size = prompt("You can choose upto 16 squares vertically and horizontally, how many would you like?");
-var all_squares = $grid_size * $grid_size;
+// sets the grid size to users choice
+var $grid_size = alert("Choose a number of squares. below 60 for better usage.");
+grid_reset($grid_size);
+// takes user input and squares it for the size of the grid
 
-  for (var i = 0; i < all_squares; i++){
-    $(".grid").append('<div class="square"></div>');
+//a function that generates random colours
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 
-  };
+};
 
-  var square_dimension = 500 / $grid_size
-  $(".square").css({"width" : square_dimension, "height" : square_dimension});
+// function for fade effect
 
-  $("#reset").click(function(){
-    location.reload("true");
+function fadeInColor(){
+  $(".square").mouseenter(function(){
+    $(this).fadeTo(0, 0);
+  });
+  $(".square").mouseenter(function(){
+    $(this).fadeTo(600, 1);
+  });
+}
+
+// a function that generates a fresh grid
+  function grid_reset(){
+    $(".grid").empty();
+    new_grid($grid_size);
+    };
+
+  function new_grid($grid_size){
+    var $grid_size =prompt("How many squares would you like?");
+    var size = 500 / $grid_size;
+    var all_squares = $grid_size * $grid_size;
+          for (var i = 0; i < all_squares; i++){
+      $(".grid").append('<div class="square"></div>');
+      }
+        $(".square").width(size);
+        $(".square").height(size);
+
+      };
+
+  $("#reset-normal").click(function(){
+    grid_reset($grid_size);
+    $(".square").hover(function(){
+      $(this).css({"background-color" : "orange"});
+    });
   });
 
+  $("#reset-randomColor").click(function(){
+    grid_reset($grid_size);
+    $(".square").hover(function(){
+      $(this).css({"background-color" : getRandomColor()});
+    });
+
+  });
+
+  $("#reset-fadeInColor").click(function(){
+    grid_reset();
+    fadeInColor();
+    });
+
   $(".square").hover(function(){
-    $(this).css({"background-color" : "red"});
+    $(this).css({"background-color" : "orange"});
   });
 
 
